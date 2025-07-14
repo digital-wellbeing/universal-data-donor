59c27a72a6dfc13982dcc5946b78948ffd77725b
Tamas Foldes <foldes.andrei@gmail.com>
2025-07-14 13:20:26 +0100

    Fix React Router basename for GitHub Pages

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ConfigProvider>
-      <BrowserRouter>
+      <BrowserRouter basename="/universal-data-donor">
         <App />
       </BrowserRouter>
     </ConfigProvider>
